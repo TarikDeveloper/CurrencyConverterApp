81a67b245720e50017e08c787f4a6c63ebd4367b
Tarikvillalobos <tarik.villalobos@gmail.com>
2023-04-18 13:53:27 -0300

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Tarik Villalobos on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
